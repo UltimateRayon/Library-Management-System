4dcb0e515cb49ff3bc86d276ccdfc4c2ce3304d4
SamiAzraf2000 <160338511+SamiAzraf2000@users.noreply.github.com>
2025-06-20 01:16:28 +0600

    Update schema.sql

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     id VARCHAR(50) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     phone VARCHAR(15) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(64) NOT NULL
 );
 
 --  book_collection table (id changed to string form)
